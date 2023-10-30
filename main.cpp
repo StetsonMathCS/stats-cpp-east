23fdad0c407c531dfcb3e0a2e94796ec5ace2990
Matt cooper <mattcooper@Matts-MacBook-Pro.local>
2023-10-30 11:14:16 -0400

    taken from west

--- main.cpp
+++ main.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include "mode_calculator.h"
+
+int main() {
+    ModeCalculator calculator;
+    float val;
+    while(std::cin >> val) {
+        calculator.handleValue(val);
+    }
+
+    float mode = calculator.getResult();
+
+    std::cout << "The mode is: " << mode << std::endl;
+
+    return 0;
+}#include <iostream>
 #include "min_calculator.h"
 
 int main() {
